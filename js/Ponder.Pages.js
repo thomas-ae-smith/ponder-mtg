d928eb9fc63006b2f1dc6c426a6079d77604c7e9
taes1g09 <taes1g09@ecs.soton.ac.uk>
2013-01-13 06:26:06 +0000

    Things are kinda working. This is nice.

--- js/Ponder.Pages.js
+++ js/Ponder.Pages.js
@@ -15,91 +15,46 @@ Ponder.module('Pages', function(Pages, App, Backbone, Marionette, $, _){
 		region : App.footer,
 	});
 
-	Pages.Link = Backbone.Model.extend({
-		defaults : {
-			link : '',
-			linkroute : ''
-		}
-	});
-
-	Pages.LinkList = Backbone.Collection.extend({ 
-		model : Pages.Link,
-		url : '/api/links',
-		initialize : function() {
-			// this.add([{'link' : 'Home', 'linkroute' : '/'}]);
-			console.log('new collection created');
-		}
-	});
-
-	Pages.NavBar = Backbone.Marionette.CompositeView.extend({
+	Pages.NavBar = Backbone.Marionette.ItemView.extend({
+		tagName : 'ul',
 		template : '#template-navbar',
 		// region : App.navbar,
 		events : {
 			 // 'click' : 'fbLogin'
 		},
-		ItemViewContainer : '#nav-list',
-		ItemView : Marionette.ItemView.extend({
-			tagName : 'li',
-			template : '#template-navbar-item',
-			events : { 'click' : 'onClick' },
-			onClick : function() {
-				Backbone.history.navigate(this.model.get('route'));
-			}
-		}),
-		emptyView : Marionette.ItemView.extend({
-			template: "#template-workspace-loading"
-		}),
-		collection : new Pages.LinkList(),
 		initialize : function() {
-			console.log("view initied");
-			this.collection.fetch();
+			FB.init({
+				appId      : '249511971845829', // App ID from the App Dashboard
+				channelUrl : '//'+window.location.hostname+'/channel.php', // Channel File for x-domain communication
+				status     : true, // check the login status upon init?
+				cookie     : true, // set sessions cookies to allow your server to access the session?
+				xfbml      : true  // parse XFBML tags on this page?
+			});
+			FB.getLoginStatus(function(response) {
+				if (response.status === 'connected') {
+					console.log('connected');
+				} else if (response.status === 'not_authorized') {
+					console.log('not_authorized');
+				} else {
+					console.log('not_logged_in');
+				}
+			});
+			console.log('fbinit called');
 		},
-		onRender : function() {
-			console.log("view rendered");
-			this.collection.add({'link' : 'Home', 'route' : '/'}, {silent:true});
-		}	
-		// 	// this.collection.add(//[
-		// 	// 	// {'link' : 'Home', 'route' : '/'},
-		// 	// 	// {'link' : 'Login', 'route' : '/login'},
-		// 	// 	// {'link' : 'Library', 'route' : '/search'},
-		// 	// 	// {'link' : 'Workspace', 'route' : '/workspace'},
-		// 	// 	// {'link' : 'Gallery', 'route' : '/decks'},
-		// 	// 	{'link' : 'Editor', 'route' : '/editor'}
-		// 	// // ]);
-		// 	// );
-
-		// 	FB.init({
-		// 		appId      : '249511971845829', // App ID from the App Dashboard
-		// 		channelUrl : '//'+window.location.hostname+'/channel.php', // Channel File for x-domain communication
-		// 		status     : true, // check the login status upon init?
-		// 		cookie     : true, // set sessions cookies to allow your server to access the session?
-		// 		xfbml      : true  // parse XFBML tags on this page?
-		// 	});
-		// 	FB.getLoginStatus(function(response) {
-		// 		if (response.status === 'connected') {
-		// 			console.log('connected');
-		// 		} else if (response.status === 'not_authorized') {
-		// 			console.log('not_authorized');
-		// 		} else {
-		// 			console.log('not_logged_in');
-		// 		}
-		// 	});
-		// 	console.log('fbinit called');
-		// },
-		// fbLogin : function() {
-		// 	FB.login(function(response) {
-		// 		if (response.authResponse) {
-		// 			(function testAPI() {
-		// 				console.log('Welcome!  Fetching your information.... ');
-		// 				FB.api('/me', function(response) {
-		// 					console.log('Good to see you, ' + response.name + '.');
-		// 				});
-		// 			}());
-		// 		} else {
-		// 			// cancelled
-		// 		}
-		// 	});
-		// }
+		fbLogin : function() {
+			FB.login(function(response) {
+				if (response.authResponse) {
+					(function testAPI() {
+						console.log('Welcome!  Fetching your information.... ');
+						FB.api('/me', function(response) {
+							console.log('Good to see you, ' + response.name + '.');
+						});
+					}());
+				} else {
+					// cancelled
+				}
+			});
+		}
 
 	});
 

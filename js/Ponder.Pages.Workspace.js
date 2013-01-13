466c78c5800de6d0b775028df00707a5ea85e634
taes1g09 <taes1g09@ecs.soton.ac.uk>
2013-01-13 03:55:57 +0000

    Further necessary image renaming and presentation work.

--- js/Ponder.Pages.Workspace.js
+++ js/Ponder.Pages.Workspace.js
@@ -6,7 +6,46 @@ Ponder.module('Pages.Workspace', function(Workspace, App, Backbone, Marionette, 
 			types : [],
 			cost : '',
 			pt : '',
-			rules : ''
+			rules : '',
+			cmc : '',
+			mana : '',
+			ci : '',
+			hybrid : false
+		},
+		events : {
+			'change' : 'onChange'
+		},
+		processCost : function() {
+			function img(code) { return "<img src=\"\/img\/Mana" + code + ".gif\">"; }
+			var cost = this.get('cost');
+			var cmc = 0;
+			var tags = '';
+			var ci = {};
+			for (var i = 0; i < cost.length; i++) {
+				switch (cost[i]) {
+					case 'X': tags += img(cost[i].toLowerCase()); break;
+					case 'W': case 'U': case 'B': case 'R': case 'G': case 'S': 
+						tags += img(cost[i].toLowerCase()); cmc +=1; ci[cost[i]] = true; break;
+					case '(': tags += img(cost[i+1] + cost[i+3]); cmc += (cost[i+1] == '2')? 2 : 1;
+						if(isNaN(cost[i+1])){ ci[cost[i+1]] = true;}; if(cost[i+3] != 'p'){ ci[cost[i+3]] = true; }; i += 4; break;
+					default: tags += (isNaN(cost[i+1]))? img(cost[i]) : img((cost[i]*10) + cost[i+1]*1);
+						cmc += (isNaN(cost[i+1]))? cost[i]*1 : (cost[i]*10) + cost[i+1]*1; i += (isNaN(cost[i+1]))? 0 : 1;
+				}
+			};
+			ci = _.keys(ci).join('') || 0;
+			this.set({'cmc' : cmc, 'mana' : tags, 'ci' : ci, 'hybrid' : (ci && ci == ci.toLowerCase()), silent : true });
+		},
+		onChange : function(model) {
+			if (model.get('cost')) {
+				this.processCost();
+			}
+			// console.log(this.get('name'), "processed:", this.get('mana'));
+		},
+		initialize : function() {
+			this.on('change', this.onChange);
+			this.onChange(this);
+			// this.onChange(this);
+			// this.set({'name' : 'test'});
 		}
 	});
 
@@ -22,32 +61,6 @@ Ponder.module('Pages.Workspace', function(Workspace, App, Backbone, Marionette, 
 	Workspace.CardItemView = Marionette.ItemView.extend({
 		tagName : 'li',
 		template : '#template-workspace-cardItemView',
-		templateHelpers : {
-			getCMC : function(cost) {
-				var cmc = 0;
-				for (var i = 0; i < cost.length; i++) {
-					switch (cost[i]) {
-						case 'X': break;
-						case 'W': case 'U': case 'B': case 'R': case 'G': case 'S': cmc += 1; break;
-						case '(': cmc += (cost[i+1] == '2')? 2 : 1; i += 4; break;
-						default: cmc += (isNaN(cost[i+1]))? cost[i]*1 : (cost[i]*10) + cost[i+1]*1;
-					}
-				};
-				return cmc;
-			},
-			renderMana : function(cost) {
-				function img(code) { return "<img src=\"\/img\/Mana" + code + ".gif\">"; }
-				var tags = '';
-				for (var i = 0; i < cost.length; i++) {
-					switch (cost[i]) {
-						case 'X': case 'W': case 'U': case 'B': case 'R': case 'G': case 'S': tags += img(cost[i].toLowerCase()); break;
-						case '(': tags += img(cost[i+1] + cost[i+3]); i += 4; break;
-						default: tags += (isNaN(cost[i+1]))? img(cost[i]) : img((cost[i]*10) + cost[i+1]*1);
-					}
-				};
-				return tags;
-			}
-		},
 		className : 'card',
 		// UI bindings create cached attributes that
 		// point to jQuery selected objects
@@ -65,6 +78,10 @@ Ponder.module('Pages.Workspace', function(Workspace, App, Backbone, Marionette, 
 
 		onRender : function() {
 			var card = this.$el;
+			var colours = { '0' : 'lightgray', 'w' : 'cornsilk', 'W' : 'wheat', 
+				'u' : 'lightblue', 'U' : 'blue', 'b' : 'gray', 'B' : 'black', 
+				'r' : 'lightcoral', 'R' : 'red', 'g' : 'darkseagreen', 'G' : 'green', 
+				'm' : '#E1B74D', 'M' : 'goldenrod', 'l' : 'burlywood', 'L' : 'sienna' };
 			this.ui.collapse.on('show', function () {
 				card.animate({'border-radius': '10px'});
 			})
@@ -72,6 +89,45 @@ Ponder.module('Pages.Workspace', function(Workspace, App, Backbone, Marionette, 
 			this.ui.collapse.on('hide', function () {
 				card.animate({'border-radius': '0px'});
 			})
+			var ci = this.model.get('ci');
+			var types = this.model.get('types');
+			console.log(types);
+			if ($.inArray('Land', types) > -1) {
+				var rules = this.model.get('rules');
+				ci += ($.inArray('Plains', types) > -1 || rules.indexOf('{W}') > -1)? 'W' : '';
+				ci += ($.inArray('Island', types) > -1 || rules.indexOf('{U}') > -1)? 'U' : '';
+				ci += ($.inArray('Swamp', types) > -1 || rules.indexOf('{B}') > -1)? 'B' : '';
+				ci += ($.inArray('Mountain', types) > -1 || rules.indexOf('{R}') > -1)? 'R' : '';
+				ci += ($.inArray('Forest', types) > -1 || rules.indexOf('{G}') > -1)? 'G' : '';
+			}
+			console.log(ci);
+			// card.css({'background-color' : colours[ci.toLowerCase()]});
+			switch (ci.length) {
+				case 1:
+					card.css({'background-color' : colours[ci.toLowerCase()]});
+					card.find('hr').css({'background-color' : colours[ci.toUpperCase()]});
+					break;
+				case 2:
+					if (this.model.get('hybrid')) {
+						card.css({'background' : '-webkit-gradient(linear, 0 0, 100% 0, from(' + colours[ci[0]] + '), to(' + colours[ci[1]] + '))' });
+					} else {
+						card.css({'background-color' : colours['m']});
+					}
+					card.find('hr').css({'background' : '-webkit-gradient(linear, 0 0, 100% 0, from(' + colours[ci[0].toUpperCase()] + '), to(' + colours[ci[1].toUpperCase()] + '))'});
+					break;
+				default:
+					card.css({'background-color' : colours['m']});
+					card.find('hr').css({'background-color' : colours['M']});
+			}
+			if ($.inArray('Artifact', types) > -1) {
+				card.css({'background-color' : colours['0']});
+				if (!ci) { card.find('hr').css({'background-color' : colours['b']}); };
+			}
+			if ($.inArray('Land', types) > -1) {
+				card.css({'background-color' : colours['l']});
+				if (!ci) { card.find('hr').css({'background-color' : colours['L']}); };
+			}
+
 		}
 
 	});
